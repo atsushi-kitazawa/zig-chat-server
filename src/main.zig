82bbee82c05ed40bbbd8f6fe7b89720545e3207b
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-08-01 23:57:02 +0900

    improve process function.

--- src/main.zig
+++ src/main.zig
@@ -28,24 +28,35 @@ pub fn doMain() anyerror!void {
 
     while (true) {
         const connection = try server.accept();
+        log.info("accept client = {}", .{connection.address});
         try process(connection);
     }
 }
 
 pub fn process(conn: net.StreamServer.Connection) anyerror!void {
+    var clientAddress_alloc = std.heap.page_allocator;
+    const clientAddress = try std.fmt.allocPrint(clientAddress_alloc, "{}>", .{conn.address});
+    defer clientAddress_alloc.free(clientAddress);
+
     while (true) {
+        _ = try conn.stream.write(clientAddress);
+
         var buf: [1024]u8 = undefined;
         var msgSize = try conn.stream.read(buf[0..]);
-        var msg = buf[0..msgSize];
-        log.info("client message is {s}", .{msg});
+        // remove \r\n
+        var msg = buf[0 .. msgSize - 2];
+        log.info("client message = {s}, size = {d}", .{ msg, msgSize });
 
-        if (std.mem.eql(u8, msg, "quit")) {
-            log.info("equal", .{});
+        if (std.mem.eql(u8, msg, "/leave")) {
+            log.info("leave client = {}", .{conn.address});
             defer conn.stream.close();
             break;
         }
 
-        _ = try conn.stream.write("hello !!");
+        var allocator = std.heap.page_allocator;
+        const respMsg = try std.fmt.allocPrint(allocator, "you sent msg = {s}\r\n", .{msg});
+        defer allocator.free(respMsg);
+        _ = try conn.stream.write(respMsg);
     }
 }
 
